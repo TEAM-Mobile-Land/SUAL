891b07f5915aaaae611511c424aa340e5b5c4c2d
최민식 <guapito231@naver.com>
2025-05-15 01:06:00 +0900

    수정: Server 폴더 추가

---
+++ Server/README.md
@@ -0,0 +1,3 @@
+# SUAL
+---
+![image](https://github.com/user-attachments/assets/bf25bb0e-6dad-4083-8ae1-a3f1a46f223d)
---
+++ Server/src/main/java/SSLBypass.java
@@ -0,0 +1,24 @@
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+
+public class SSLBypass {
+    public static void disableSslVerification() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
