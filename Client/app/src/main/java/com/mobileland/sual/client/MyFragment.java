9bc5594df5da2444788caa56d373c74f30c504a1
eoyan <nabong1031@gmail.com>
2025-05-16 15:48:28 +0900

    feat: 네비게이션 바 액티비티 추가

---
+++ Client/app/src/main/java/com/mobileland/sual/client/MyFragment.java
@@ -0,0 +1,21 @@
+package com.mobileland.sual.client;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment extends Fragment {
+    public MyFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+}
---
+++ Client/app/src/main/java/com/mobileland/sual/client/SaveFragment.java
@@ -0,0 +1,21 @@
+package com.mobileland.sual.client;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SaveFragment extends Fragment {
+    public SaveFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_save, container, false);
+    }
+}
