3b8cf126d8ebec475e63cba2e5cdbf1fd6b38666
Brdy8294 <hsoo1203@naver.com>
2025-05-30 11:39:00 +0900

    AsyncTask를 사용하여 데이터베이스 작업을 백그라운드 스레드로 이동, 콜백 인터페이스 추가로 비동기 결과 처리, 리소스 관리를 위한 try finally 블록 추가

--- Client/app/src/main/java/com/mobileland/sual/client/manager/PreferenceManager.java
+++ Client/app/src/main/java/com/mobileland/sual/client/manager/PreferenceManager.java
@@ -1,44 +1,92 @@
 package com.mobileland.sual.client.manager;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import com.mobileland.sual.client.database.PreferenceDao;
 import com.mobileland.sual.client.model.UserPreference;
 import java.util.List;
 
+@SuppressWarnings("deprecation")
 public class PreferenceManager {
     private PreferenceDao preferenceDao;
+    private Context context;
 
     public PreferenceManager(Context context) {
+        this.context = context;
         preferenceDao = new PreferenceDao(context);
     }
 
-    public void updateUserPreference(String category, int score, String keyword) {
-        preferenceDao.open();
-        UserPreference preference = preferenceDao.getPreferenceByCategory(category);
-
-        if (preference == null) {
-            preference = new UserPreference(category, score, keyword);
-            preferenceDao.insertPreference(preference);
-        } else {
-            preference.setPreferenceScore(score);
-            preference.setKeyword(keyword);
-            preferenceDao.updatePreference(preference);
-        }
-        preferenceDao.close();
+    public void updateUserPreference(final String category, final int score, final String keyword) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                preferenceDao.open();
+                try {
+                    UserPreference preference = preferenceDao.getPreferenceByCategory(category);
+                    if (preference == null) {
+                        preference = new UserPreference(category, score, keyword);
+                        preferenceDao.insertPreference(preference);
+                    } else {
+                        preference.setPreferenceScore(score);
+                        preference.setKeyword(keyword);
+                        preferenceDao.updatePreference(preference);
+                    }
+                } finally {
+                    preferenceDao.close();
+                }
+                return null;
+            }
+        }.execute();
     }
 
-    public int getPreferenceScore(String category) {
-        preferenceDao.open();
-        UserPreference preference = preferenceDao.getPreferenceByCategory(category);
-        preferenceDao.close();
-
-        return preference != null ? preference.getPreferenceScore() : 0;
+    public interface OnPreferenceLoadedListener {
+        void onPreferenceLoaded(int score);
     }
 
-    public List<UserPreference> getPreferencesByKeyword(String keyword) {
-        preferenceDao.open();
-        List<UserPreference> preferences = preferenceDao.getPreferencesByKeyword(keyword);
-        preferenceDao.close();
-        return preferences;
+    public void getPreferenceScore(final String category, final OnPreferenceLoadedListener listener) {
+        new AsyncTask<Void, Void, Integer>() {
+            @Override
+            protected Integer doInBackground(Void... voids) {
+                preferenceDao.open();
+                try {
+                    UserPreference preference = preferenceDao.getPreferenceByCategory(category);
+                    return preference != null ? preference.getPreferenceScore() : 0;
+                } finally {
+                    preferenceDao.close();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Integer score) {
+                if (listener != null) {
+                    listener.onPreferenceLoaded(score);
+                }
+            }
+        }.execute();
+    }
+
+    public void getPreferencesByKeyword(final String keyword, final OnPreferencesLoadedListener listener) {
+        new AsyncTask<Void, Void, List<UserPreference>>() {
+            @Override
+            protected List<UserPreference> doInBackground(Void... voids) {
+                preferenceDao.open();
+                try {
+                    return preferenceDao.getPreferencesByKeyword(keyword);
+                } finally {
+                    preferenceDao.close();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(List<UserPreference> preferences) {
+                if (listener != null) {
+                    listener.onPreferencesLoaded(preferences);
+                }
+            }
+        }.execute();
+    }
+
+    public interface OnPreferencesLoadedListener {
+        void onPreferencesLoaded(List<UserPreference> preferences);
     }
 }
