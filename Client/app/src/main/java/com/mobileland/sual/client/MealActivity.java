019376e3f2752f84178078e882424ea86eb8c1e3
eoyan <nabong1031@gmail.com>
2025-06-05 17:33:09 +0900

    feat: 학식 페이지에 닫기 버튼 추가 및 홈 액티비티로 이동 처리

--- Client/app/src/main/java/com/mobileland/sual/client/MealActivity.java
+++ Client/app/src/main/java/com/mobileland/sual/client/MealActivity.java
@@ -1,6 +1,9 @@
 package com.mobileland.sual.client;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
@@ -19,6 +22,14 @@ public class MealActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
+
+        ImageButton closeBtn = findViewById(R.id.closeMealButton);
+        closeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MealActivity.this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        });
 
         MealPagerAdapter adapter = new MealPagerAdapter(this);
         viewPager.setAdapter(adapter);
